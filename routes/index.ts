e65b53098baed430dd74b98b56bb72c5883e2bcf
Craig Alexander <craig.d.alexander@gmail.com>
2017-03-09 18:46:18 -0500

    cleanup

--- routes/index.ts
+++ routes/index.ts
@@ -32,8 +32,6 @@ router.post('/', (req: express.Request, res: express.Response, next: express.Nex
 
   let date = request.result.parameters.date || (today.getFullYear() + '-' +  ('0' + (today.getMonth()+1)).slice(-2) + '-' + ('0' + today.getDate()).slice(-2));
 
-  console.log(date);
-
   getContent(lunchApiUrl + date)
   .then((jsonData) => {
     let menuItem: MenuItem = JSON.parse(jsonData);
